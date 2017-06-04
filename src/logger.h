a501632f7000e4997ba3d596758b2883143403fe
Daner Jimenez <danerjimenez@gmail.com>
2017-06-05 01:08:32 +0800

    Revert "v1.0.0: Can transfer files on Windows"
    This reverts commit 26e892fff9d2abee64abbce32e8e9f7d0cb7b887.

--- src/logger.h
+++
@@ -1,6 +0,0 @@
-#ifndef LOGGER_H_INCLUDED
-#define LOGGER_H_INCLUDED
-
-bool logresult(const result_t* result);
-
-#endif // LOGGER_H_INCLUDED
--- src/truck.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef TRUCK_H_INCLUDED
-#define TRUCK_H_INCLUDED
-
-result_t* deliver(const task_t* task);
-
-#endif // TRUCK_H_INCLUDED
